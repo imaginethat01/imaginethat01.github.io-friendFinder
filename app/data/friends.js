a95778838a8fc8c4f6ea668e84544961de221fd2
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-05-27 21:14:36 -0400

    upwards onwards

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+
+var friends = [
+{
+"name":"Bruce Springstien",
+  "photo":"ht  tps://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+"name":"Charlie Chaplin",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      4,
+      1,
+      8,
+      9,
+      5,
+      2,
+      3,
+      8,
+      2
+    ]
+  },
+  {
+    "name":"Soren Kierkegaard",
+      "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "scores":[
+          7,
+          8,
+          4,
+          5,
+          8,
+          2,
+          7,
+          3,
+          5,
+          8
+        ]
+      }
+]
+  
