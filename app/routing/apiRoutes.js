5788d6c32e47dd777422e8c85f6e9b7213bd109e
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-05-29 21:16:12 -0400

    integration of working code. simplification.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,17 @@
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
-  
-  // Displays all characters
-  app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
-  
-  // Displays a single character, or returns false
-  app.get("/api/friends/:friend", function(req, res) {
-    var chosen = req.params.friend;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < friends.length; i++) {
-      if (chosen === friends[i].routeName) {
-        return res.json(friends[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
-  // Create New Characters - takes in JSON input
-  app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newfriend = req.body;
-  
-    console.log(newfriend);
-  
-    // We then add the json the user sent to the character array
-    friends.push(newfriend);
-  
-    // We then display the JSON to the users
-    res.json(newfriend);
-  });
+
+var friendList = require('../data/friends.js');
+var path = require('path');
+
+//==============================================
+//ROUTING
+//==============================================
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friendList);
+	});
+
+	app.post('/api/friends', function(req, res){
+		friendList.push(req.body);
+	})
+};
--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+//=============================================================
+// Dependencies
+// ============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+
+//=============================================================
 // Sets up the Express App
-// =============================================================
+//=============================================================
+
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
-  
+
+//=============================================================
+//Routing
+//=============================================================
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+
+// =============================================================
+// Starts the server to begin listening 
+// =============================================================
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT ' + process.env.PORT);
+});
