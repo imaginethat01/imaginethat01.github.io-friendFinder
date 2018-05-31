84a1d0279995a17ad39991b50e0202f882c965ef
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-05-31 15:24:03 -0400

    before class

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,52 +2,100 @@
 var friends = [
 {
 "name":"Bruce Springstien",
-  "photo":"ht  tps://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
+      3,
       2,
       5,
-      4,
+      3,
+      1,
+      1,
+      5,
+      2,
+      3,
       1
     ]
 },
 {
 "name":"Charlie Chaplin",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"https://pbs.twimg.com/profile_images/701734065431560192/9shia0aV_400x400.jpg",
   "scores":[
-      5,
-      4,
       1,
-      8,
-      9,
-      5,
-      2,
+      4,
       3,
-      8,
+      3,
+      2,
+      5,
+      5,
+      5,
+      3,
       2
     ]
   },
   {
     "name":"Soren Kierkegaard",
-      "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "photo":"https://www.danishmuseum.org/images/exhibitions/kierkegaard.jpg",
       "scores":[
-          7,
-          8,
+          3,
+          4,
           4,
           5,
-          8,
-          2,
-          7,
           3,
-          5,
-          8
+          2,
+          1,
+          2,
+          3,
+          4
         ]
-      }
+      },
+      {
+        "name":"Mina Loy",
+          "photo":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1328027884i/9665445._UY400_SS400_.jpg",
+          "scores":[
+              3,
+              4,
+              4,
+              5,
+              2,
+              1,
+              4,
+              2,
+              5,
+              4
+            ]
+          },
+          {
+            "name":"Barbara Walters",
+              "photo":"https://pbs.twimg.com/profile_images/148206104/BW_photo_400x400.jpg",
+              "scores":[
+                  3,
+                  5,
+                  4,
+                  3,
+                  2,
+                  1,
+                  5,
+                  3,
+                  2,
+                  1
+                ]
+              },
+              {
+                "name":"Marisa Dal Santo",
+                  "photo":"https://i.pinimg.com/736x/0b/50/61/0b50618a37ebd1ba06692ec0d62414fc--skater-girls-surf.jpg",
+                  "scores":[
+                      5,
+                      1,
+                      2,
+                      3,
+                      3,
+                      3,
+                      4,
+                      5,
+                      5,
+                      1
+                    ]
+                  },
 ]
   
 module.exports = friends;
